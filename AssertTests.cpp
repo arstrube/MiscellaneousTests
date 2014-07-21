9753873c2b3bf85c6f3dd8e5b0faf9eba0b91890
Arnd R. Strube <a.strube@wind-soft.de>
2014-07-21 13:06:21 +0200

    This doesn't work yet.
    The reason is that we should fail (in case of the paramters don't match)
    while in the mock(), but we don't know whether we failed or not until
    after the mock returned.

--- AssertTests.cpp
+++ AssertTests.cpp
@@ -1,38 +1,42 @@
 #include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/TestRegistry.h"
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockSupport.h"
 #include "CppUTestExt/MockSupportPlugin.h"
+#include <cstdio>
 
 /** ASSERT callback and macros */
 
 void X_onAssert(char const * const file, int line);
-
+/*
 #define X_DEFINE_THIS_FILE \
     static char const l_this_file[] = __FILE__;
-
+*/
 #define X_DEFINE_THIS_MODULE(name_) \
-    static char_t const l_this_file[] = name_;
+    static char const l_this_file[] = name_;
 
 #define X_ASSERT(test_) \
         ((test_) ? (void)0 : X_onAssert(&l_this_file[0], (int)__LINE__))
 
-#define Q_ASSERT_ID(id_, test_) \
+#define X_ASSERT_ID(id_, test_) \
         ((test_) ? (void)0 : X_onAssert(&l_this_file[0], (int)(id_)))
 
 
-X_DEFINE_THIS_FILE
+/* X_DEFINE_THIS_FILE */
 X_DEFINE_THIS_MODULE("AssertTests")
 
-static void X_onAssert(char const * const file, int line)
+void X_onAssert(char const * const file, int line)
 {
-    mock().actualCall().withParameter("file", file).withParameter("line", line);
-    FAIL("ASSERT failed")
+    mock().actualCall("X_onAssert").withParameter("file", file).withParameter("line", line);
+    fprintf(stderr, "Assertion failed in %s, line %d", file, line);
+    fflush(stdout);
+    FAIL("");
 }
 
-void functionToTest(void * ptr)
+void functionToTest(char * ptr)
 {
-    X_ASSERT(0 != ptr);
-    ptr[2] = 0;
+    X_ASSERT_ID(1, 0 != ptr);
+    ptr[1] = 0;
 }
 
 TEST_GROUP(Group)
@@ -41,12 +45,15 @@ TEST_GROUP(Group)
 
 TEST(Group, Test)
 {
-    mock()
+    mock().expectOneCall("X_onAssert").withParameter("file", "AssertTests")
+          .withParameter("line", 1);
+    functionToTest(0);
 }
 
 int main(int ac, char** av)
 {
-
+    MockSupportPlugin mockPlugin;
+    TestRegistry::getCurrentRegistry()->installPlugin(&mockPlugin);
 	return CommandLineTestRunner::RunAllTests(ac, av);
 }
 
