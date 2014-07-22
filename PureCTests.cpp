d5dedcebf09589f087207e170ac980d9deee4c68
Arnd R. Strube <a.strube@wind-soft.de>
2014-07-22 13:52:58 +0200

    PureCTests: Same tests (using proposed changes).

--- PureCTests.cpp
+++ PureCTests.cpp
@@ -2,16 +2,12 @@
 #include "CppUTest/TestHarness_c.h"
 
 /** For each C test group */
-TEST_GROUP_C(mygroup)
-{
-    TEST_GROUP_C_SETUP_WRAPPER(mygroup);
-    TEST_GROUP_C_TEARDOWN_WRAPPER(mygroup);
-};
+TEST_GROUP_C_WRAPPER(mygroup);
 
 /** For each C test */
-TEST_GROUP_C_WRAPPER(mygroup, test_success);
-TEST_GROUP_C_WRAPPER(mygroup, test_mockfailure);
-TEST_GROUP_C_WRAPPER(mygroup, test_equalfailure);
+TEST_C_WRAPPER(mygroup, test_success);
+TEST_C_WRAPPER(mygroup, test_mockfailure);
+TEST_C_WRAPPER(mygroup, test_equalfailure);
 
 /** Test main as usual */
 int main(int ac, char** av)
