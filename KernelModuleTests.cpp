43bd57d57cd1e0e8c5108d25cd1351b22b0f9730
Arnd R. Strube <a.strube@wind-soft.de>
2014-08-15 11:11:17 +0200

    KernelModuleTests: Complete "Hello 1" example.
    KernelModule will always by compiled with special
    kernel makefile.

--- KernelModuleTests.cpp
+++ KernelModuleTests.cpp
@@ -2,39 +2,53 @@
 
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
-#include "KernelModule.h"
+#include "CppUTest/SimpleString.h"
 #include <cstdarg>
 #include "CppUTest/PlatformSpecificFunctions.h"
 
 #define NBUF 2048L
-static char buffer[NBUF];
+static char log_buffer[NBUF];
+static char log_level[3];
 
 extern "C" {
 
-    /** Kernel function stubs */
+    #include "KernelModule/KernelModule.h"
+    #include "linux/kern_levels.h"
 
-    int printk (const char * fmt, ...);
+    /** Kernel function stubs */
 
     int printk (const char * fmt, ...)
     {
         va_list args;
-        va_start( args, fmt );
-        return PlatformSpecificVSNprintf(buffer, NBUF, fmt, args);
+        SimpleString::StrNCpy(log_level, fmt, 2);
+        fmt += 2;
+        va_start(args, fmt);
+        return PlatformSpecificVSNprintf(log_buffer, NBUF, fmt, args);
     }
 
 }
 
-
-TEST_GROUP(KernelModule) {};
+TEST_GROUP(KernelModule)
+{
+    void setup()
+    {
+        log_buffer[0] = 0;
+        log_level[0] = 0;
+    }
+};
 
 TEST(KernelModule, Init)
 {
-    FAIL("Not implemented.")
+    LONGS_EQUAL(0, init_module());
+    STRCMP_EQUAL(KERN_INFO, log_level);
+    STRCMP_EQUAL("Hello world 1.\n", log_buffer);
 }
 
 TEST(KernelModule, Cleanup)
 {
-    FAIL("Not implemented.")
+    cleanup_module();
+    STRCMP_EQUAL(KERN_INFO, log_level);
+    STRCMP_EQUAL("Goodbye world 1.\n", log_buffer);
 }
 
 int main(int ac, char** av)
