5013f1b9adbb3831735f2dd6befc9235be36ad1d
Arnd R. Strube <a.strube@wind-soft.de>
2014-08-15 10:09:30 +0200

    Stub printk()

--- KernelModuleTests.cpp
+++ KernelModuleTests.cpp
@@ -3,11 +3,26 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
 #include "KernelModule.h"
+#include <cstdarg>
+#include "CppUTest/PlatformSpecificFunctions.h"
+
+#define NBUF 2048L
+static char buffer[NBUF];
 
 extern "C" {
+
+    /** Kernel function stubs */
+
+    int printk (const char * fmt, ...);
+
+    int printk (const char * fmt, ...)
+    {
+        va_list args;
+        va_start( args, fmt );
+        return PlatformSpecificVSNprintf(buffer, NBUF, fmt, args);
+    }
+
 }
-
-
 
 
 TEST_GROUP(KernelModule) {};
