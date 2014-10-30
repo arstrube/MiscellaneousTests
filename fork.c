264fcb42f20466ec68507d75a594dc4e1481d0c7
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-30 10:45:16 +0100

    fork: first shot.

---
+++ fork.c
@@ -0,0 +1,55 @@
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    pid_t cpid, w;
+    int status;
+
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cpid == 0) {            /* Code executed by child */
+        printf("Child PID is %ld\n", (long) getpid());
+        if (argc == 1)
+            pause();                  /* Wait for signals */
+        int arg = atoi(argv[1]);
+        switch(arg) {
+            case -1 : { int a=10, b=0; a=a/b; break; }
+            case -2 : { *(int*)0=0; break; }
+        }
+        _exit(arg);
+
+    } else {                   /* Code executed by parent */
+        do {
+            w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);
+            if (w == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+
+            if (WIFEXITED(status)) {
+                printf("exited, status=%d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                #ifdef WCOREDUMP
+                if (WCOREDUMP(status))
+                    printf("killed with coredump by signal %d\n", WTERMSIG(status));
+                else
+                #endif
+                    printf("killed by signal %d\n", WTERMSIG(status));
+            } else if (WIFSTOPPED(status)) {
+                printf("stopped by signal %d\n", WSTOPSIG(status));
+            } else if (WIFCONTINUED(status)) {
+                printf("continued\n");
+            }
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        exit(EXIT_SUCCESS);
+    }
+}
+
