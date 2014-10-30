b8394d81946504646cc7e9b1f069acee0793acf2
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-30 10:45:38 +0100

    CrashReporterTests: Pass & Fail.

--- CrashReporterTests.cpp
+++ CrashReporterTests.cpp
@@ -2,7 +2,8 @@
 #include "CppUTest/TestHarness.h"
 
 TEST_GROUP(SeparateProcessBug) {};
-TEST(SeparateProcessBug, Success) { CHECK(false); }
+TEST(SeparateProcessBug, Fail) { CHECK(false); }
+TEST(SeparateProcessBug, Pass) { CHECK(true); }
 
 int main(int ac, char** av) {
     return CommandLineTestRunner::RunAllTests(ac, av);
