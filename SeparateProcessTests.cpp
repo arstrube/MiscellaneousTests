c45174c62dc1a45bb3351d07a13d31c665fee70d
Arnd R. Strube <a.strube@wind-soft.de>
2014-12-26 14:26:13 +0100

    SeparateProcessTests: Remove trailing spaces.

--- SeparateProcessTests.cpp
+++ SeparateProcessTests.cpp
@@ -1,11 +1,11 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
 
-extern "C" 
+extern "C"
 {
 }
 
-struct Data 
+struct Data
 {
     Data(int value) : value_(value) {}
     int value_;
@@ -15,22 +15,22 @@ TEST_GROUP(Separate_Process)
 {
 };
 
-TEST(Separate_Process, thisTestIsStillRunAndSucceeds) 
+TEST(Separate_Process, thisTestIsStillRunAndSucceeds)
 {
     LONGS_EQUAL(1, 1);
 }
-TEST(Separate_Process, thisTestIsStillRunAndFails) 
+TEST(Separate_Process, thisTestIsStillRunAndFails)
 {
     LONGS_EQUAL(3, 1);
 }
 
-TEST(Separate_Process, thisTestCrashesAlso) 
+TEST(Separate_Process, thisTestCrashesAlso)
 {
 	Data* data = NULL;
 	data->value_ = 15;
 }
 
-TEST(Separate_Process, STRCMP_EQUAL_CrashesWithNULL) 
+TEST(Separate_Process, STRCMP_EQUAL_CrashesWithNULL)
 {
 	STRCMP_EQUAL("Hello", NULL);
 }
