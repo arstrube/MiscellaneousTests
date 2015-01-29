057a78f75c0e1252ece2fdea0ac7f63456cbbc82
Arnd R. Strube <a.strube@wind-soft.de>
2015-01-29 13:38:46 +0100

    CoverageTests: Eliminate most code duplication.

--- CoverageTests_c.c
+++ CoverageTests_c.c
@@ -1,205 +1,71 @@
 #include "CoverageTests.h"
 
+#define MACRO_FUNCTION_AorB_andC(a,b,c) { \
+    int x;                                \
+    if ((a || b) && c) {                  \
+        x = 1;                            \
+    }                                     \
+    else {                                \
+        x = 2;                            \
+    }                                     \
+    return x;                             \
+}
+
+#define MACRO_FUNCTION_AandBandC(a,b,c) { \
+    int x;                                \
+    if (a && b && c) {                    \
+        x = 1;                            \
+    }                                     \
+    else {                                \
+        x = 2;                            \
+    }                                     \
+    return x;                             \
+}
+
 /** Statement Coverage (C0) ****************************************/
 
 int Statement_100_percent_AorB_andC (char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+           MACRO_FUNCTION_AorB_andC(a, b, c)
 
 int Statement_100_percent_AandBandC (char a, char b, char c)
-{
-    int x;
-    
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+           MACRO_FUNCTION_AandBandC(a, b, c)
 
 /** Gcov "Branches executed" (n/a) *********************************/
 
 int GcovBranch_100_percent_AorB_andC(char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+            MACRO_FUNCTION_AorB_andC(a, b, c)
 
 int GcovBranch_100_percent_AandBandC(char a, char b, char c)
-{
-    int x;
-    
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+            MACRO_FUNCTION_AandBandC(a, b, c)
 
 /** Branch Coverage ************************************************/
 
 int Branch_100_percent_AorB_andC(char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+        MACRO_FUNCTION_AorB_andC(a, b, c)
 
 int Branch_100_percent_AandBandC(char a, char b, char c)
-{
-    int x;
-    
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+        MACRO_FUNCTION_AandBandC(a, b, c)
 
 /** Condition Coverage *********************************************/
 
 int Condition_100_percent_AandBandC(char a, char b, char c)
-{
-    int x;
+           MACRO_FUNCTION_AandBandC(a, b, c)
     
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
-
 int Condition_100_percent_AorB_andC(char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+           MACRO_FUNCTION_AorB_andC(a, b, c)
 
 /** MC/DC **********************************************************/
 
 int MCDC_100_percent_AandBandC(char a, char b, char c)
-{
-    int x;
-    
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+      MACRO_FUNCTION_AandBandC(a, b, c)
 
 int MCDC_100_percent_AorB_andC(char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+      MACRO_FUNCTION_AorB_andC(a, b, c)
 
 /** Multi-Decision Coverage ****************************************/
 
 int Multi_100_percent_AandBandC(char a, char b, char c)
-{
-    int x;
-    
-    if (a && b && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+       MACRO_FUNCTION_AandBandC(a, b, c)
 
 int Multi_100_percent_AorB_andC(char a, char b, char c)
-{
-    int x;
-    
-    if ((a || b) && c)
-    {
-        x = 1;
-    }
-    else
-    {
-        x = 2;
-    }
-    
-    return x;
-}
+       MACRO_FUNCTION_AorB_andC(a, b, c)
