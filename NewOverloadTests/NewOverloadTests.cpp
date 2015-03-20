0ef114abdc97f514db80c3299c0938b2380c8351
Arnd R. Strube <a.strube@wind-soft.de>
2015-03-20 15:36:49 +0100

    NewOverloadTests: Make it work for GCC

--- NewOverloadTests/NewOverloadTests.cpp
+++ NewOverloadTests/NewOverloadTests.cpp
@@ -1,12 +1,6 @@
-/**
- *
- */
-
 #include <iostream>
-#include <cstdlib>
 
- __attribute__((optnone)) int main(int, char**)
-{
+extern "C" int main(int, char**) {
     int * n = new (std::nothrow) int;
     char * s = new (std::nothrow) char[5];
     delete n;
