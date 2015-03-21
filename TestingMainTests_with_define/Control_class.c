43aeefce6b22d3142d5276f606beaedd5619b54e
Arnd R. Strube <a.strube@wind-soft.de>
2015-03-21 06:24:34 +0100

    Unified both versions of test

--- TestingMainTests_with_define/Control_class.c
+++
@@ -1,9 +0,0 @@
-#include "Control_class.h"
-
-void main (void) {
-    while (1) {
-        Task1_run();
-        Task2_run();
-        Sys_sleep(5);
-    }
-}
--- TestingMainTests_with_define/Control_class.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROL_CLASS_H
-#define CONTROL_CLASS_H
-
-void Task1_run(void);
-void Task2_run(void);
-void Sys_sleep(int);
-
-#endif
