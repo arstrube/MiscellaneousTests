6c954ca4d7f9541e4317c76dd16b3bfa00b405b3
Arnd R. Strube <a.strube@wind-soft.de>
2015-03-31 08:58:51 +0200

    InlineTests: How to use static alone.

--- InlineTests/forced.h
+++ InlineTests/forced.h
@@ -1,8 +1,21 @@
-#ifndef INLINE_MACROS_H
-#define INLINE_MACROS_H
+#ifndef FORCED_H
+#define FORCED_H
+
+#if 0 // This actually inlines the functions
 
 #define STATIC static
 #define INLINE inline
 #define ATTRIBUTE(a) __attribute__((a))
 
-#endif // INLINE_MACROS_H
+#else // This simply makes them static
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+
+#define STATIC static
+#define INLINE
+#define ATTRIBUTE(a)
+
+#endif // #if 0 or #if 1
+
+#endif // FORCED_H
