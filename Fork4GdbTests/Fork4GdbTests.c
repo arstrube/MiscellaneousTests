cb1c2c5aed0332e728f22364e4a937ba62a7f265
Arnd R. Strube <a.strube@wind-soft.de>
2015-04-25 21:49:42 +0200

    Fork4GdbTests: Incorporate child_run() into main()

--- Fork4GdbTests/Fork4GdbTests.c
+++ Fork4GdbTests/Fork4GdbTests.c
@@ -3,16 +3,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int child_run(void) {
-    return *(volatile int*) 0; /* should terminate with signal 11 */
-}
-
 int main(int ac, char** av) {
     int status;
     pid_t cpid = fork();
-    if(0 == cpid) {
-        (void) child_run();
-    } else {
+    if(0 == cpid) { /* Child */
+        return *(volatile int*) 0; /* exits with signal 11 */
+    } else { /* Parent */
         do {
             waitpid(cpid, &status, WUNTRACED);
             if(WIFSIGNALED(status)) {
