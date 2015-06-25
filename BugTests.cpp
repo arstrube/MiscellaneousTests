9b1a6ef9ad67ffe83940752f95cc311e8616dc26
Arnd R. Strube <a.strube@wind-soft.de>
2015-06-25 14:45:10 +0200

    BugTests: Add the real McCoy.

--- BugTests.cpp
+++ BugTests.cpp
@@ -1,19 +1,46 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 #include <CppUTestExt/MockSupport.h>
+
 //--------------------------------------------------------------------------------------------
-struct Simple
-{
-    Simple()
-    {
+struct Simple {
+    Simple() {
         m_data = new int;
     }
-    ~Simple()
-    {
+    Simple(const Simple& rhs) {
+        m_data = new int;
+        *m_data = *rhs.m_data;
+    }
+    Simple& operator=(const Simple& rhs) {
+        *m_data = *rhs.m_data;
+        return *this;
+    }
+    ~Simple() {
         delete m_data;
     }
-private:
     int * m_data;
+};
+
+struct Simple2 {
+    int m_data;
+};
+
+class SimpleComparator : public MockNamedValueComparatorWithCopy {
+public:
+    virtual bool isEqual(const void* object1, const void* object2)
+    {
+        return object1 == object2;
+    }
+    virtual SimpleString valueToString(const void* object)
+    {
+        return StringFrom(((Simple*)object)->m_data);
+    }
+    virtual void* copy(const void* to, const void* from)
+    {
+        Simple* ret = (Simple*)to;
+        *ret = *((Simple*)from);
+        return (void*)ret;
+    }
 };
 //--------------------------------------------------------------------------------------------
 TEST_GROUP(Simple) {};
@@ -21,15 +48,34 @@ TEST_GROUP(Simple) {};
 TEST(Simple, WillItFail)
 {
     Simple source, destination;
+    *source.m_data = 5;
+    *destination.m_data = 7;
+    mock().expectOneCall("foo").withOutputParameterOfTypeReturning("Simple", "simple", &source);
+    mock().actualCall("foo").withOutputParameterOfType("Simple", "simple", &destination);
 
+    mock().checkExpectations();
+    LONGS_EQUAL(5, *destination.m_data);
+
+    mock().clear();
+}
+TEST(Simple, WillItFail2)
+{
+    Simple2 source, destination;
+    source.m_data = 5;
+    destination.m_data = 7;
     mock().expectOneCall("foo").withOutputParameterReturning("simple", &source, sizeof(source));
     mock().actualCall("foo").withOutputParameter("simple", &destination);
 
     mock().checkExpectations();
+    LONGS_EQUAL(5, destination.m_data);
+
     mock().clear();
 }
 //--------------------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
+    SimpleComparator comparator;
+    mock().installComparator("Simple", comparator);
     return CommandLineTestRunner::RunAllTests(argc, argv);
+    mock().removeAllComparators();
 }
