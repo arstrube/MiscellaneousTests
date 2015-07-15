93bab2a3d406ae8567d541a4acd4d06d5ef6050f
Arnd R. Strube <a.strube@wind-soft.de>
2015-07-15 12:03:51 +0200

    FETests: Improve demo tests.

--- FETests/FEDemoTests.cpp
+++ FETests/FEDemoTests.cpp
@@ -1,30 +1,3 @@
-/*
- * Copyright (c) 2015, Michael Feathers, James Grenning, Bas Vodde
- * and Arnd Strube. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE EARLIER MENTIONED AUTHORS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/TestRegistry.h"
@@ -32,9 +5,7 @@
 #include <cfenv>
 #include <limits>
 
-extern "C" { 
-    #include "FETests_c.h"
-}
+typedef float float32;
 
 TEST_GROUP(FE_Demo) {
     void setup(void) override {
@@ -42,16 +13,19 @@ TEST_GROUP(FE_Demo) {
     }
 };
 TEST(FE_Demo, should_fail_when__FE_DIVBYZERO__is_set) {
-    DOUBLES_EQUAL(std::numeric_limits<double>::infinity(), 1.0d / 0.0d, 0.0d);
+    CHECK(std::numeric_limits<float>::infinity() == 1.0f / 0.0f);
 }
 TEST(FE_Demo, should_fail_when__FE_UNDERFLOW__is_set) {
-    set_underflow_c();
+    float32 f = 0.01f;
+    while (f > 0.0f) f *= f;
+    CHECK(f == 0.0f);
 }
-TEST(FE_Demo, should_succeed_FE_INEXACT_set) {
-    DOUBLES_EQUAL(5.2f, 15.6f / 3.0f, 0.000001f);
+TEST(FE_Demo, should_fail_when__FE_INEXACT_is_set) {
+    CHECK(5.100001f > 15.3f / 3.0f);
+    CHECK(5.099999f < 15.3f / 3.0f);
 }
 TEST(FE_Demo, should_succeed_when_no_flags_are_set) {
-    DOUBLES_EQUAL(5.0f, 15.0f / 3.0f, 0.0f);
+    CHECK(5.0f == 15.0f / 3.0f);
 }
 
 int main(int ac, char** av) {
