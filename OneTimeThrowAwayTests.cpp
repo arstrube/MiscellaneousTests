931c2922291ad7ee775484c80eb7a115fd613e94
Arnd R. Strube <a.strube@wind-soft.de>
2015-08-19 15:55:16 +0200

    OneTime: The Leak Thing - this also leaks

--- OneTimeThrowAwayTests.cpp
+++ OneTimeThrowAwayTests.cpp
@@ -1,20 +1,13 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness_c.h"
-#include <stdexcept>
 
 TEST_GROUP(CHECK_EXCEPTION) {};
 
-void test_me(void) {
-    CHECK_THROWS(std::range_error, throw std::range_error(""));
-}
-
 TEST(CHECK_EXCEPTION, aTest) {
-    MemoryLeakWarningPlugin::turnOnNewDeleteOverloads();
-    test_me();
+    MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
 };
 
 int main(int argc, char** argv)
 {
-    crash_on_allocation_number(2);
     return CommandLineTestRunner::RunAllTests(argc, argv);
 }
