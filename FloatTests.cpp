6ddb30c0d5d13ca321d73f2c072981228931074e
Arnd R. Strube <a.strube@wind-soft.de>
2016-01-18 15:49:18 +0000

    FloatTests: Some changes to original code.

--- FloatTests.cpp
+++ FloatTests.cpp
@@ -1,46 +1,37 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/CommandLineTestRunner.h"
 
-//#define _DEBUG
-//#include "Float_t.h"
-
-union Double_t
-{
-    Double_t(double num = 0.0l) : d(num) {}
+union Double_t {
+    Double_t(const double num = 0.0l) : d(num) {}
     double d;
-    struct
-    {
+    struct {
         uint64_t mantissa : 52;
         uint64_t exponent : 11;
         uint64_t sign : 1;
     } parts;
 };
 
-double PlatformSpecificGetNaN() 
-{
+double PlatformSpecificGetNaN() {
     Double_t d;
-    d.parts.exponent = 0x3FF;
+    d.parts.exponent = 0x7ff;
     d.parts.mantissa = 1;
     return d.d;
 }
 
-TEST_GROUP(double)
-{
+TEST_GROUP(double) {
 };
 
-TEST(double, isnan)
-{
+TEST(double, isnan) {
     const double zero = 0.0;
     const double not_a_number = zero / zero;
     const Double_t mynan = Double_t(PlatformSpecificGetNaN());
-    LONGS_EQUAL(0x3ff, (uint32_t) mynan.parts.exponent )
-    LONGS_EQUAL(0x1, (uint32_t) mynan.parts.mantissa )
-    DOUBLES_EQUAL(not_a_number, PlatformSpecificGetNaN(), 0.0);
+    LONGS_EQUAL(0x7ff, mynan.parts.exponent )
+    LONGS_EQUAL(0x1, mynan.parts.mantissa )
+    DOUBLES_EQUAL(not_a_number, mynan.d, 0.0);
 }
 
 /** Test main as usual */
-int main(int ac, char** av)
-{
+int main(int ac, char** av) {
     return CommandLineTestRunner::RunAllTests(ac, av);
 }
 
