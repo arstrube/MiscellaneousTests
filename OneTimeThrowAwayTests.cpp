1b985d8948a7ea0ea41bd692d72e8452ecd71c53
Arnd R. Strube <a.strube@wind-soft.de>
2016-03-04 09:14:53 +0000

    Test assert() in OneTimeThrowAwayTests.cpp exceptions w. text

--- OneTimeThrowAwayTests.cpp
+++ OneTimeThrowAwayTests.cpp
@@ -8,10 +8,16 @@
     try { f; } catch (char const* e) { assert_triggered = true; } \
     CHECK(assert_triggered); \
 }
+#define CHECK_ASSERT_FAILED_WITH_TEXT(f, s) \
+{ \
+    SimpleString actual; \
+    try { f; } catch (char const* e) { actual = e; } \
+    STRCMP_EQUAL(s, actual.asCharString()); \
+}
 
 // Test double for assert()
 void assert(bool condition) {
-    ACTUAL_ASSERT_TEXT("");
+    ACTUAL_ASSERT_TEXT("assert() failed");
 }
 
 // Production code
@@ -24,6 +30,10 @@ TEST_GROUP(useful) {};
 
 TEST(useful, null_pointer_should_trigger_assert) {
     CHECK_ASSERT_FAILED(my_thing(0));
+}
+
+TEST(useful, null_pointer_should_trigger_assert_with_text) {
+    CHECK_ASSERT_FAILED_WITH_TEXT(my_thing(0), "assert() failed");
 }
 
 int main(int argc, char** argv) {
