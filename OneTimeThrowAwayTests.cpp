5b4af91e9734df1c57839563bfc187180fafe975
Arnd R. Strube <a.strube@wind-soft.de>
2016-03-24 09:49:03 +0100

    master: OneTimeThrowAwayTests for debugging Ieee754 errors

--- OneTimeThrowAwayTests.cpp
+++ OneTimeThrowAwayTests.cpp
@@ -1,48 +1,10 @@
-#include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
-#include "CppUTestExt/MockSupport.h"
+#include "CppUTestExt/IEEE754ExceptionsPlugin.h"
 
-// In a header file:
-typedef struct
-{
-    uint8_t *data;
-    uint16_t data_length;
-} network_dto;
+static volatile float f = 1.0;
+static volatile IEEE754ExceptionsPlugin plugin; // link it
 
-typedef void (*net_rx_callback)(network_dto dto);
-
-void network_setup(net_rx_callback cb);
-
-// In my mock file:
-void network_setup(net_rx_callback cb)
-{
-    mock("network").setData("cb", (void*)cb);
-    mock("network").actualCall("network_setup");
-}
-
-// In my test:
-net_rx_callback callback_pointer;
-
-void test_callback(network_dto)
-{
-     // whatever
-}
-
-TEST_GROUP(network) {};
-
-TEST(network, callback_test)
-{
-    mock("network")
-         .expectOneCall("network_setup")
-         .ignoreOtherParameters();
-
-    network_setup(test_callback);
-
-    callback_pointer = (net_rx_callback)(mock("network").getData("cbx").getPointerValue());
-
-    mock().clear();
-}
-
-int main(int argc, char** argv) {
-    return RUN_ALL_TESTS(argc, argv);
+int main(int, char**) {
+    f /= 0.0f;
+    return 0;
 }
